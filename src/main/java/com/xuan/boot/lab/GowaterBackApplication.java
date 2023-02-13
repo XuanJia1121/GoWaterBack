b695faa5152ee849b25f3497453a197e2f62029c
xuanjia <xuanjia961121@gmail.com>
2023-02-13 17:22:45 +0800

    firstcommit

---
+++ src/main/java/com/xuan/boot/lab/GowaterBackApplication.java
@@ -0,0 +1,13 @@
+package com.xuan.boot.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowaterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowaterBackApplication.class, args);
+	}
+
+}
