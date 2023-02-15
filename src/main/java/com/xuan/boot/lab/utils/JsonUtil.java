d7a27be83a42bc91c0b51ec90da92b4de304d988
xuanjia <xuanjia961121@gmail.com>
2023-02-15 16:58:03 +0800

    add jsonutil

---
+++ src/main/java/com/xuan/boot/lab/utils/JsonUtil.java
@@ -0,0 +1,20 @@
+package com.xuan.boot.lab.utils;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+	
+	public static String objectToJson(Object obj) throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+	
+	public static Object jsonToObject(String json,Class<?> clazz) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+	
+}
--- src/main/java/com/xuan/boot/lab/utils/JwtUtil.java
+++ src/main/java/com/xuan/boot/lab/utils/JwtUtil.java
@@ -2,19 +2,24 @@ package com.xuan.boot.lab.utils;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JwtUtil {
 
 	public final static String JWT_KEY = "MySecret";
-
+	public final static String JWT_PREFIX = "Bearer ";
+	public final static String HEADER = "Authorization";
+	
 	public static String genToken(String username) {
 		Date expireDate = new Date(System.currentTimeMillis() + 5 * 60 * 1000);
-		return Jwts.builder()
+		String token =  Jwts.builder()
 				.setSubject(username)
 				.setExpiration(expireDate)
 				// MySecret是自訂的私鑰，HS512是自選的演算法，可以任意改變
 				.signWith(SignatureAlgorithm.HS512, JWT_KEY).compact();
+		return StringUtils.join(JWT_PREFIX + token);
 	}
 }
