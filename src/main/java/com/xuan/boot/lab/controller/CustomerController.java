cf6bfff11b97714bd5ec1244da3e10b7a89cfc3f
xuanjia <xuanjia961121@gmail.com>
2023-02-16 12:00:46 +0800

    空白測試

--- src/main/java/com/xuan/boot/lab/controller/CustomerController.java
+++ src/main/java/com/xuan/boot/lab/controller/CustomerController.java
@@ -1,14 +1,8 @@
 package com.xuan.boot.lab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.xuan.boot.lab.dto.CustomerDto;
 import com.xuan.boot.lab.service.CustomerService;
 
 @RestController
@@ -17,7 +11,7 @@ public class CustomerController {
 	
 	@Autowired
 	CustomerService customerService;
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "哈哈哈摟";
