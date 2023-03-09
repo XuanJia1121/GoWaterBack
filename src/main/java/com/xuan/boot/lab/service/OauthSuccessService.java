64c024c68a27a71181de34d0e02a94768caddf7f
xuanjia <xuanjia961121@gmail.com>
2023-03-09 17:19:56 +0800

    add oauth2 setting

--- src/main/java/com/xuan/boot/lab/service/OauthSuccessService.java
+++ src/main/java/com/xuan/boot/lab/service/OauthSuccessService.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -15,10 +14,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OauthSuccessService implements AuthenticationSuccessHandler {
-
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-		System.out.println("oauth login success");
+		RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+		redirectStrategy.sendRedirect(request, response,"http://localhost:8080/oauth2/success");
 	}
 	
 }
