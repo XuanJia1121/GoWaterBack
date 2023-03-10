12850b71afd8f3066d36a901555c443ef1a03250
xuanjia <xuanjia961121@gmail.com>
2023-03-10 16:57:26 +0800

    fix cors setting

--- src/main/java/com/xuan/boot/lab/conf/SecurityConfiguration.java
+++ src/main/java/com/xuan/boot/lab/conf/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.xuan.boot.lab.conf;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.xuan.boot.lab.enums.UrlPatten;
 import com.xuan.boot.lab.filter.CustomUsernamePasswordAuthenticationFilter;
@@ -86,5 +90,16 @@ public class SecurityConfiguration {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	} 
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 	
 }
